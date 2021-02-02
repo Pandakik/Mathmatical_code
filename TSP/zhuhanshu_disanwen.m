0bbf38736c4917409e8c95f6bc1eb3c056dc2941
Pandakik <71925880+Pandakik@users.noreply.github.com>
2021-02-02 16:50:00 +0800

    Add files via upload

---
+++ TSP/zhuhanshu_disanwen.m
@@ -0,0 +1,11 @@
+data1 = xlsread('F:\潘宗勇PANZONGYONG\2020暑期冲啊\数学建模冲啊\集训\第四个任务\2020杭电大学生数学建模竞赛集训模型4\B题：无线可充电传感器网络充电路线规划\B题附件3.xlsx');
+x = data1(:,1);
+y = data1(:,2);
+C = data1(:,1:2);
+NC_max=400;
+m=32;
+Alpha=1;
+Beta=1;
+Rho=0.2;
+Q=20;
+[R_best,L_best,L_ave,Shortest_Route,Shortest_Length]=ACATSP(C,NC_max,m,Alpha,Beta,Rho,Q);
